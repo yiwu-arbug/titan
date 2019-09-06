8dd40774cdf7214fbb6216db5d4d5d1986c37680
Yi Wu <yiwu@pingcap.com>
2019-09-06 00:07:33 +0000

    revert db_bench secondary_db
    Signed-off-by: Yi Wu <yiwu@pingcap.com>

--- tools/db_bench_tool.cc
+++ tools/db_bench_tool.cc
@@ -768,19 +768,6 @@ DEFINE_uint64(blob_db_bytes_per_sync, 0, "Bytes to sync blob file at.");
 DEFINE_uint64(blob_db_file_size, 256 * 1024 * 1024,
               "Target size of each blob file.");
 
-// Secondary DB instance Options
-DEFINE_bool(use_secondary_db, false,
-            "Open a RocksDB secondary instance. A primary instance can be "
-            "running in another db_bench process.");
-
-DEFINE_string(secondary_path, "",
-              "Path to a directory used by the secondary instance to store "
-              "private files, e.g. info log.");
-
-DEFINE_int32(secondary_update_interval, 5,
-             "Secondary instance attempts to catch up with the primary every "
-             "secondary_update_interval seconds.");
-
 #endif  // ROCKSDB_LITE
 
 DEFINE_bool(report_bg_io_stats, false,
@@ -2949,12 +2936,6 @@ class Benchmark {
       }
     }
 
-    if (secondary_update_thread_) {
-      secondary_update_stopped_.store(1, std::memory_order_relaxed);
-      secondary_update_thread_->join();
-      secondary_update_thread_.reset();
-    }
-
 #ifndef ROCKSDB_LITE
     if (name != "replay" && FLAGS_trace_file != "") {
       Status s = db_.db->EndTrace();
@@ -2974,22 +2955,10 @@ class Benchmark {
                   ->ToString()
                   .c_str());
     }
-
-#ifndef ROCKSDB_LITE
-    if (FLAGS_use_secondary_db) {
-      fprintf(stdout, "Secondary instance updated  %" PRIu64 " times.\n",
-              secondary_db_updates_);
-    }
-#endif  // ROCKSDB_LITE
   }
 
  private:
   std::shared_ptr<TimestampEmulator> timestamp_emulator_;
-  std::unique_ptr<port::Thread> secondary_update_thread_;
-  std::atomic<int> secondary_update_stopped_{0};
-#ifndef ROCKSDB_LITE
-  uint64_t secondary_db_updates_ = 0;
-#endif  // ROCKSDB_LITE
   struct ThreadArg {
     Benchmark* bm;
     SharedState* shared;
@@ -3647,11 +3616,6 @@ class Benchmark {
       fprintf(stderr, "Cannot use readonly flag with transaction_db\n");
       exit(1);
     }
-    if (FLAGS_use_secondary_db &&
-        (FLAGS_transaction_db || FLAGS_optimistic_transaction_db)) {
-      fprintf(stderr, "Cannot use use_secondary_db flag with transaction_db\n");
-      exit(1);
-    }
 #endif  // ROCKSDB_LITE
   }
 
@@ -3888,32 +3852,6 @@ class Benchmark {
       s = titandb::TitanDB::Open(options, db_name, &ptr);
       if (s.ok()) {
         db->db = ptr;
-      }
-    } else if (FLAGS_use_secondary_db) {
-      if (FLAGS_secondary_path.empty()) {
-        std::string default_secondary_path;
-        FLAGS_env->GetTestDirectory(&default_secondary_path);
-        default_secondary_path += "/dbbench_secondary";
-        FLAGS_secondary_path = default_secondary_path;
-      }
-      s = DB::OpenAsSecondary(options, db_name, FLAGS_secondary_path, &db->db);
-      if (s.ok() && FLAGS_secondary_update_interval > 0) {
-        secondary_update_thread_.reset(new port::Thread(
-            [this](int interval, DBWithColumnFamilies* _db) {
-              while (0 == secondary_update_stopped_.load(
-                              std::memory_order_relaxed)) {
-                Status secondary_update_status =
-                    _db->db->TryCatchUpWithPrimary();
-                if (!secondary_update_status.ok()) {
-                  fprintf(stderr, "Failed to catch up with primary: %s\n",
-                          secondary_update_status.ToString().c_str());
-                  break;
-                }
-                ++secondary_db_updates_;
-                FLAGS_env->SleepForMicroseconds(interval * 1000000);
-              }
-            },
-            FLAGS_secondary_update_interval, db));
       }
 #endif  // ROCKSDB_LITE
     } else {
