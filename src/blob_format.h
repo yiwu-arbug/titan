559b98431d44fc8b79c71418205e10c3e9e67f71
Yi Wu <yiwu@pingcap.com>
2019-09-20 18:31:48 +0000

    Revert "revert sample_for_compression"
    This reverts commit 425f2a9f7bd9bcd9db3f97fa001dc20f78af8aeb.

--- src/blob_format.h
+++ src/blob_format.h
@@ -37,7 +37,8 @@ class BlobEncoder {
   BlobEncoder(CompressionType compression)
       : compression_ctx_(compression),
         compression_info_(compression_opt_, compression_ctx_,
-                          CompressionDict::GetEmptyDict(), compression) {}
+                          CompressionDict::GetEmptyDict(), compression,
+                          0 /*sample_for_compression*/) {}
 
   void EncodeRecord(const BlobRecord& record);
 
